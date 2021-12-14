4b3ebcf83eb3c7e2549ea3a3d27551eee7b17fbc
owerbat <niktimak@gmail.com>
2021-12-14 19:44:40 +0300

    Simple copy

--- main.cpp
+++ main.cpp
@@ -4,11 +4,34 @@
 #include <oneapi/tbb/parallel_for.h>
 #include <oneapi/tbb/blocked_range.h>
 #include <oneapi/tbb/task_arena.h>
+#include <oneapi/tbb/tick_count.h>
+
+void simple_copy(float ** const src, float * const dst, const size_t n_rows, const size_t n_cols);
 
 int main() {
-    
+    constexpr size_t n_rows = 4 * 1024 * 1024;
+    constexpr size_t n_cols = 64;
+
+    float ** const col_matrix = new float*[n_cols];
+    for (size_t i = 0; i < n_cols; ++i)
+        col_matrix[i] = new float[n_rows];
+
+    float * const continuous_row_matrix = new float[n_rows * n_cols];
+
+    oneapi::tbb::tick_count t0 = oneapi::tbb::tick_count::now();
+    simple_copy(col_matrix, continuous_row_matrix, n_rows, n_cols);
+    oneapi::tbb::tick_count t1 = oneapi::tbb::tick_count::now();
+    std::cout << "Simple copy: " << (t1 - t0).seconds() << std::endl;
 
     return 0;
+}
+
+void simple_copy(float ** const src, float * const dst, const size_t n_rows, const size_t n_cols) {
+    for (size_t i = 0; i < n_rows; ++i) {
+        for (size_t j = 0; j < n_cols; ++j) {
+            dst[i * n_cols + j] = src[j][i];
+        }
+    }
 }
 
 void test_tbb() {
